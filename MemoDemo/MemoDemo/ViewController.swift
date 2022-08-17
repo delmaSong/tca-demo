8153cffb1ab4977d394cfef987357a34a0f9789f
dayeong.song <sdy2856@gmail.com>
2022-08-17 17:02:26 +0900

    chore: 프로젝트 생성

---
+++ MemoDemo/MemoDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoDemo
+//
+//  Created by Devsisters on 2022/08/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
