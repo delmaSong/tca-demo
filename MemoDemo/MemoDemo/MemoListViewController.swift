9d50767fbcd8c291d8f4f8844cce63779dd909ea
dayeong.song <sdy2856@gmail.com>
2022-08-22 09:25:57 +0900

    feat: MemoListViewController 생성

---
+++ MemoDemo/MemoDemo/MemoListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MemoListViewController.swift
+//  MemoDemo
+//
+//  Created by delma on 2022/08/22.
+//
+
+import UIKit
+
+class MemoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
--- MemoDemo/MemoDemo/SceneDelegate.swift
+++ MemoDemo/MemoDemo/SceneDelegate.swift
@@ -12,7 +12,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        
+        let viewController = MemoListViewController()
+        let navigation = UINavigationController(rootViewController: viewController)
+        
+        window.rootViewController = navigation
+        
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
 }
