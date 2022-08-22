790f866d822db8d9c7f7b3c76283e305dd3c2e2f
dayeong.song <sdy2856@gmail.com>
2022-08-22 10:14:56 +0900

    feat: 테이블뷰 추가

--- MemoDemo/MemoDemo/MemoListViewController.swift
+++ MemoDemo/MemoDemo/MemoListViewController.swift
@@ -7,10 +7,33 @@
 
 import UIKit
 
-class MemoListViewController: UIViewController {
+import SnapKit
 
+final class MemoListViewController: UIViewController {
+
+    private let memoListTableView: UITableView = {
+        let view = UITableView(frame: .zero, style: .plain)
+        view.register(MemoListTableViewCell.self, forCellReuseIdentifier: MemoListTableViewCell.identifier)
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        configure()
     }
+    
+    private func configure() {
+        view.backgroundColor = .white
+        
+        navigationController?.navigationBar.topItem?.title = "목록"
+        
+        view.addSubview(memoListTableView)
+        
+        memoListTableView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.leading.trailing.bottom.equalTo(view)
+        }
+    }
+
 }
