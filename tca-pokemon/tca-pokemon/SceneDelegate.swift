84d00a2127b715313f9a2eb69b21c42924874a9d
dayeong.song <sdy2856@gmail.com>
2022-08-30 10:58:29 +0900

    feat: 스토리보드 제거

--- tca-pokemon/tca-pokemon/SceneDelegate.swift
+++ tca-pokemon/tca-pokemon/SceneDelegate.swift
@@ -8,11 +8,19 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        
+        let viewController = ViewController()
+        let navigation = UINavigationController(rootViewController: viewController)
+        
+        window.rootViewController = navigation
+        
+        self.window = window
+        window.makeKeyAndVisible()
     }
 }
-
