dd2d023d5e470e05101e4260590a575389522b74
dayeong.song <sdy2856@gmail.com>
2022-08-31 10:43:06 +0900

    feat: 포켓몬 정보를 표시할 PokemonInfoState 생성

---
+++ tca-pokemon/tca-pokemon/PokemonInfo.swift
@@ -0,0 +1,16 @@
+//
+//  PokemonInfo.swift
+//  tca-pokemon
+//
+//  Created by Devsisters on 2022/08/30.
+//
+
+import Foundation
+
+struct PokemonInfoState {
+    var name: String
+    var stat: String
+    var type: String
+    var isLiked: Bool
+    var imageURL: String
+}
